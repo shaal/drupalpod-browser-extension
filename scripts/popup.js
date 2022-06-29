4a85ce359b5b2e3316a008445f2c025e1e0eb504
Darren Oh <darren@oh.name>
2022-06-29 16:27:49 -0400

    Add latest official Drupal release

--- scripts/popup.js
+++ scripts/popup.js
@@ -109,7 +109,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const moduleVersionStatus = document.getElementById('module-version');
         moduleVersionStatus.innerHTML = pageResults.moduleVersion;
 
-        const drupalCoreVersionsArray = ['9.4.0', '9.3.x', '9.4.x', '9.5.x', '10.0.x', '10.1.x'];
+        const drupalCoreVersionsArray = ['9.4.1', '9.3.x', '9.4.x', '9.5.x', '10.0.x', '10.1.x'];
         const drupalInstallProfiles = ['(none)', 'standard', 'demo_umami', 'minimal'];
         const availablePatchesArray = getPatchesFromLinks(pageResults.allHrefs);
 
